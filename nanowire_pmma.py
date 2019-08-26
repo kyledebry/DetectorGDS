06060705d9b93fcf49613466adaa1955c0d5a121
Kyle DeBry <debry.1@osu.edu>
2019-08-25 22:50:59 -0400

    Completed high-resolution area design for PMMA

--- nanowire_pmma.py
+++ nanowire_pmma.py
@@ -8,7 +8,8 @@ space_width = pitch - wire_width
 active_diameter = 15
 lined_area = 60
 active_radius = active_diameter / 2
-lead_width = 1
+lead_width = 2
+taper_distance = 10
 
 def add_line(cell: gdspy.Cell, y, side):
     x = side * lined_area / 2
@@ -17,6 +18,11 @@ def add_line(cell: gdspy.Cell, y, side):
         end_x = lead_width / 2
         end_x = math.copysign(end_x, x)
         start_x = -end_x
+        if abs(y) - active_radius < taper_distance:
+            shift = -turn_width * (1 - (abs(y) - active_radius) / taper_distance) / 2
+            shift = math.copysign(shift, start_x)
+            start_x += shift
+            end_x -= shift
     else:
         end_x = math.sqrt(max(active_radius ** 2 - y ** 2, 0))
         end_x = math.copysign(end_x, -x)
@@ -56,12 +62,20 @@ def make_nanowire_meander():
 
 def make_circle():
     circle = gdspy.Cell('CIRCLE')
-    left_circle = gdspy.RobustPath((-turn_width, -active_radius), width=pitch, ends='round')
-    left_circle.arc(active_radius, 3 * math.pi / 2, math.pi / 2)
-    circle.add(left_circle)
-    right_circle = gdspy.RobustPath((turn_width, active_radius), width=pitch, ends='round')
-    right_circle.arc(active_radius, math.pi / 2, -math.pi / 2)
-    circle.add(right_circle)
+    left_path = gdspy.RobustPath((-lead_width / 2, -lined_area / 2), width=2 * pitch, ends='round')
+    left_path.segment((-lead_width / 2, -active_radius - taper_distance))
+    left_path.segment((-turn_width, -active_radius - pitch * 0.75))
+    left_path.arc(active_radius + pitch * 0.75, 3 * math.pi / 2, math.pi / 2)
+    left_path.segment((-lead_width / 2, active_radius + taper_distance))
+    left_path.segment((-lead_width / 2, lined_area / 2))
+    circle.add(left_path)
+    right_path = gdspy.RobustPath((lead_width / 2, lined_area / 2), width=2 * pitch, ends='round')
+    right_path.segment((lead_width / 2, active_radius + taper_distance))
+    right_path.segment((turn_width, active_radius + pitch * 0.75))
+    right_path.arc(active_radius + pitch * 0.75, math.pi / 2, -math.pi / 2)
+    right_path.segment((lead_width / 2, -active_radius - taper_distance))
+    right_path.segment((lead_width / 2, -lined_area / 2))
+    circle.add(right_path)
 
     return circle
 
